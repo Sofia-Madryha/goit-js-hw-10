914bc203ad6c4c129d09c574257ff8a16ba3c549
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-08-31 02:41:52 +0300

    add homework

---
+++ src/js/cat-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_VDJSMJPG2m48BcCykoUZMpUcPLXl6OLIir4W5GDRotTQALTA6O9sdi92sADBJGdi';
+
+function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(err => error(err));
+}
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(err => error(err));
+}
+
+export { fetchBreeds, fetchCatByBreed };
